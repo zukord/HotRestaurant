846f28f9bd6037890f90c6f53a169c4a57df53ea
David Zukor <zukord@gmail.com>
2019-04-06 13:12:55 -0400

    Tables and other changes

--- objects.js
+++ objects.js
@@ -9,14 +9,15 @@ var tableArray = [
     }
      
 ];
-var waitingList = [
-    customerName: "James",
-    customerEmail: "tagorda@gmail.com",
+var waitingList = [{
+    customerName: "omar",
+    customerEmail: "ota@gmail.com",
     customerID: "100",
-    customerTelephone: "320-555-6700"
+    customerTelephone: "320-555-6700",
     numberOfParty: "",
     specialRequest: "highTable"
-]
+
+}]
 
 module.exports = tableArray;
 module.exports = waitingList;
